1d74f7f91e6ed770850e2bfc90eb6506c41d707d
WarpBubble <110014045+WarpBubble@users.noreply.github.com>
2023-03-21 08:40:53 +1300

    creation of function handlers

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,15 +1,59 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { table } from "console";
+import { v4 } from "uuid";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "ProductsTable";
+
+export const CreateProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqBody = JSON.parse(event.body as string);
+
+  const product = {
+    ...reqBody,
+    productID: v4(),
+  };
+
+  await docClient
+    .put({
+      TableName: "ProductsTable",
+      Item: {
+        product,
+      },
+    })
+    .promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+    body: JSON.stringify(product),
+  };
+};
+
+
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        productId: id,
       },
-      null,
-      2,
-    ),
+    })
+    .promise();
+
+    {/* if the output is not found return a status code for not found*/}
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "NOT FOUND" }),
+    };
+  }
+
+   {/* return that it has been found and the json string of detail */}
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output),
   };
 };
